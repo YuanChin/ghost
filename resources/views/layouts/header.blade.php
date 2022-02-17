399f2d104ff4f7107fa544f968757bee80a2c058
yuanchin <cs861229503@gmail.com>
2022-02-17 10:16:28 +0800

    用戶登入狀態

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -14,9 +14,35 @@
 
         <!-- Right Side Of Navbar -->
         <ul class="navbar-nav">
+            @guest
             <!-- Authentication Links -->
             <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">登入</a></li>
             <li class="nav-item"><a class="nav-link" href="{{ route('register') }}">註冊</a></li>
+            @else
+            <li class="nav-item dropdown">
+                <a class="nav-link text-gray-400 dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <img src="https://cdn.learnku.com/uploads/images/201709/20/1/PtDKbASVcz.png?imageView2/1/w/60/h/60" class="rounded-circle img-responsive img-circle" width="30px" height="30px">
+                    <span class="ml-1">{{ Auth::user()->name }}</span>
+                </a>
+                <div class="user-menu dropdown-menu bg-gray-700 mt-1" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item text-gray-50" href="#">
+                        <i class="far fa-user mr-2"></i>
+                        個人中心
+                    </a>
+                    <a class="dropdown-item text-gray-50" href="#">
+                        <i class="far fa-edit mr-2"></i>
+                        編輯資料
+                    </a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" id="logout" href="#">
+                        <form action="{{ route('logout') }}" method="POST" onsubmit="return confirm('您確定要退出嗎？');">
+                        {{ csrf_field() }}
+                        <button class="btn btn-block btn-danger" type="submit" name="button">退出</button>
+                        </form>
+                    </a>
+                </div>
+            </li>
+            @endguest
         </ul>
     </div>
 </nav>
