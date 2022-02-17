3fed27beb3dfb7ca0aa4fb175ae75d404df2afdb
yuanchin <cs861229503@gmail.com>
2022-02-17 11:20:43 +0800

    註冊與登入頁面重構且新增驗證碼功能

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,144 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <link rel="stylesheet" href="{{ mix('css/animation.css') }}">
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+    <title>登入 | Ghost 社群</title>
+</head>
+<body class="bg-gray-900">
+    <div class="container">
+        <!---remove custom style-->
+        <ul class="circles">
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+        <div class="row justify-content-center">
+            <div class="col-md-10">
+                <!-- Login Form-->
+                <div class="card border-0 rounded-none shadow-lg my-5">
+                    <div class="row no-gutters">
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+                        <div class="col-md-7">
+                            <img src="https://i0.hippopx.com/photos/740/19/598/dog-hovawart-black-pet-preview.jpg" class="w-100 h-100" alt="dog">
+                            <!-- remove custom style -->
+                            <ul class="circles">
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                            </ul>
                         </div>
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
+                        <div class="col-md-5">
+                            <div class="card-body m-3">
+                                <div class="text-center h4 mb-4 text-blue-400">
+                                    <p>登入Ghost會員</p>
                                 </div>
+                                <form method="POST" action="{{ route('login') }}">
+                                    @csrf
+
+                                    <div class="form-group">
+                                        <input type="email" class="form-control @error('email') is-invalid @enderror"
+                                               name="email" value="{{ old('email') }}" required autocomplete="email"
+                                               autofocus placeholder="請輸入您的信箱 …">
+                                        @error('email')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                    </div>
+
+                                    <div class="form-group">
+                                        <input type="password" class="form-control @error('password') is-invalid @enderror"
+                                               name="password" required autocomplete="current-password"
+                                               placeholder="請輸入您的密碼 …">
+                                        @error('password')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                    </div>
+
+                                    <div class="form-group">
+                                        <input class="form-control{{ $errors->has('captcha') ? ' is-invalid' : '' }}"
+                                               name="captcha" required placeholder="請輸入驗證碼 …">
+                                        <img class="mt-3 mb-2" src="{{ captcha_src('flat') }}"
+                                             onclick="this.src='/captcha/flat?' + Math.random()" title="點擊圖片重新獲取驗證碼">
+                                        @error('captcha')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ __('輸入驗證碼有誤！') }}</strong>
+                                            </span>
+                                        @enderror
+                                    </div>
+
+                                    <div class="form-group">
+                                        <div class="form-group form-check">
+                                            <input type="checkbox" class="form-check-input" name="remember" id="remember"
+                                                {{ old('remember') ? 'checked' : '' }}>
+                                            <label class="form-check-label" for="remember">
+                                                {{ __('記住我') }}
+                                            </label>
+                                        </div>
+                                    </div>
+
+                                    <button type="submit" class="btn btn-primary btn-block rounded-pill">
+                                                {{ __('登入') }}
+                                    </button>
+                                </form>
+
+                                <hr>
+
+                                <div class="text-center">
+                                    @if (Route::has('password.request'))
+                                        <a class="btn btn-link small" href="{{ route('password.request') }}">
+                                            {{ __('忘記您的密碼?') }}
+                                        </a>
+                                    @endif
+                                </div>
+
+                                <div class="text-center">
+                                    <a class="btn btn-link small" href="{{ route('register') }}">創建帳號點我 !</a>
+                                </div>
+
                             </div>
                         </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
+                    </div>
                 </div>
+                <!-- End -- Login Form-->
             </div>
         </div>
     </div>
-</div>
-@endsection
+
+    <!-- Scripts -->
+    <script src="{{ mix('js/app.js') }}"></script>
+</body>
+</html>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,77 +1,139 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <link rel="stylesheet" href="{{ mix('css/animation.css') }}">
 
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+    <title>會員註冊 | Ghost 社群</title>
+</head>
+<body class="bg-gray-900">
+    <div class="container">
+        <!---remove custom style-->
+        <ul class="circles">
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+        <div class="row justify-content-center">
+            <div class="col-md-10">
 
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                <!-- Register_Form start-->
+                <div class="card border-0 rounded-none shadow-lg my-5">
+                    <div class="row no-gutters">
 
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
+                        <div class="col-md-7">
+                            <img src="https://i0.hippopx.com/photos/823/780/947/dog-the-most-obvious-labrador-black-preview.jpg" class="w-100 h-100" alt="dog">
+                            <!-- remove custom style -->
+                            <ul class="circles">
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                            </ul>
                         </div>
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                        <div class="col-md-5">
+                            <div class="card-body m-2">
+                                <div class="text-center h4 mb-4 text-blue-400">
+                                    <p>註冊成為Ghost會員</p>
+                                </div>
+                                <form action="{{ route('register') }}" method="POST">
+                                    @csrf
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                                    <div class="form-group">
+                                        <input type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}"
+                                            name="name" value="{{ old('name') }}" required autofocus placeholder="請輸入您要註冊的姓名 …">
+                                        @if ($errors->has('name'))
+                                            <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $errors->first('name') }}</strong>
+                                            </span>
+                                        @endif
+                                    </div>
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+                                    <div class="form-group">
+                                        <input type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}"
+                                            name="email" value="{{ old('email') }}" required placeholder="請輸入您要註冊的信箱 …">
+                                        @if ($errors->has('email'))
+                                            <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $errors->first('email') }}</strong>
+                                            </span>
+                                        @endif
+                                    </div>
+
+                                    <div class="form-group">
+                                        <input type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}"
+                                            name="password" required placeholder="請輸入您要註冊的密碼 …">
+                                        @if ($errors->has('password'))
+                                            <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $errors->first('password') }}</strong>
+                                            </span>
+                                        @endif
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="password" class="form-control"
+                                            name="password_confirmation" required
+                                            placeholder="請再一次輸入您要註冊的密碼 …">
+                                    </div>
+
+                                    <div class="form-group">
+                                        <input class="form-control{{ $errors->has('captcha') ? ' is-invalid' : '' }}"
+                                               name="captcha" required placeholder="請輸入驗證碼 …">
+                                        <img class="mt-3 mb-2" src="{{ captcha_src('flat') }}"
+                                            onclick="this.src='/captcha/flat?' + Math.random()" title="點擊圖片重新獲取驗證碼">
+                                        @if ($errors->has('captcha'))
+                                            <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $errors->first('captcha') }}</strong>
+                                            </span>
+                                        @endif
+                                    </div>
+
+                                    <button type="submit" class="btn btn-primary btn-block rounded-pill">
+                                        {{ __('註冊') }}
+                                    </button>
+
+                                </form>
+                                <div class="text-center mt-2">
+                                    <a class="small btn-link" href="{{ route('login') }}">已經有帳號了嗎？ 點我登入！</a>
+                                </div>
                             </div>
                         </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                    </div>
                 </div>
+                <!-- Register_form end -->
+
             </div>
         </div>
+        
     </div>
-</div>
-@endsection
+
+    <!-- Scripts -->
+    <script src="{{ mix('js/app.js') }}"></script>
+</body>
+</html>
